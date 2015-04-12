aa0e40b0ecb940398a6a02ce15d73e844f1b19c6
Michael Murphy <mcmurphy91@gmail.com>
2015-04-12 15:13:20 +0100

    Updated

--- src/com/example/serviceproject/ServiceAdapter.java
+++ src/com/example/serviceproject/ServiceAdapter.java
@@ -1,9 +1,15 @@
 package com.example.serviceproject;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import android.app.Activity;
 import android.content.ClipData.Item;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.content.res.Resources;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,56 +17,105 @@ import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
 
-public class ServiceAdapter extends BaseAdapter {
+public class ServiceAdapter extends BaseAdapter implements OnClickListener {
 
-public ServiceAdapter(Context context, int textViewResourceId) {
-    super(context, textViewResourceId);
+private Activity activity;
+private ArrayList services;
+private static LayoutInflater inflater=null;
+public Resources res;
+Service temp=null;
+int i=0;
+
+public ServiceAdapter(Activity activity, ArrayList services, Resources res) {
+   activity=this.activity;
+   services=this.services;
+   res=this.res;
+   inflater=(LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 }
 
-public ServiceAdapter(Context context, int resource, List<Item> items) {
-    super(context, resource, items);
-}
 
 @Override
 public View getView(int position, View convertView, ViewGroup parent) {
 
     View v = convertView;
-
-    if (v == null) {
-
+    ViewHolder holder;
+    if (convertView == null) {
+    	holder=new ViewHolder();
         LayoutInflater vi;
-        vi = LayoutInflater.from(getContext());
-        v = vi.inflate(R.layout.rows, null);
-
+        v = inflater.inflate(R.layout.rows, null);
+        holder.text=(TextView)v.findViewById(R.id.name);
+        holder.text1=(TextView)v.findViewById(R.id.description);
+        v.setTag(holder);
     }
 
-    Item p = getItem(position);
+    else
+    {
+    	holder=(ViewHolder)v.getTag(); 
+    	
+    }
+    if(services.size()<=0)
+    {
+    	holder.text.setText("No data");
+    }
+    else
+    {
 
-    if (p != null) {
+    	 /***** Get each Model object from Arraylist ********/
+        temp=null;
+        temp = (Service ) services.get( position );
+         
+        /************  Set Model values in Holder elements ***********/
 
-        TextView name = (TextView) v.findViewById(R.id.name);
-        TextView description= (TextView) v.findViewById(R.id.description);
-        TextView url = (TextView) v.findViewById(R.id.address);
-        TextView type = (TextView) v.findViewById(R.id.type);
-
-        if (name != null) {
-           name.setText(p.getName());
-        }
-        if (description != null) {
-
-            description.setText(p.getDescription());
-        }
-        if (url != null) {
-
-            url.setText(p.getUrl());
-        }
-        if (url != null) {
-
-            type.setText(p.getType());
-        }
+         holder.text.setText( temp.getName() );
+         holder.text1.setText( temp.getAddress());
     }
 
     return v;
 
 }
+
+@Override
+public int getCount() {
+	if(services.size()<=0)
+	return 1;
+	return services.size();
 }
+
+@Override
+public Object getItem(int position) {
+	// TODO Auto-generated method stub
+	return position;
+}
+
+@Override
+public long getItemId(int position) {
+	// TODO Auto-generated method stub
+	return position;
+}
+
+@Override
+public void onClick(DialogInterface dialog, int which) {
+	Log.e("Button Clicked", "Click");
+	
+}
+}
+
+private class OnItemClickListener implements OnClickListener
+{
+	 private int mPosition;
+    OnItemClickListener(int position){
+         mPosition = position;
+    }
+     
+    @Override
+    public void onClick(View arg0) {
+
+
+      MainActivity sct = (MainActivity)activity;
+
+     /****  Call  onItemClick Method inside CustomListViewAndroidExample Class ( See Below )****/
+
+        sct.onItemClick(mPosition);
+    }
+               
+}   
---
+++ src/com/example/serviceproject/ViewHolder.java
@@ -0,0 +1,9 @@
+package com.example.serviceproject;
+
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class ViewHolder {
+	  public TextView text;
+      public TextView text1;
+}
